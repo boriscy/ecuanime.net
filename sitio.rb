4bef2c95df1a58209a3632fa6a0aec399d496863
Boris Barroso <boriscyber@gmail.com>
2010-05-21 15:51:26 -0400

    Haml

--- sitio.rb
+++ sitio.rb
@@ -1,16 +1,33 @@
 require 'rubygems'
 require 'sinatra'
-#require 'ruby-debug'
+require 'mail'
+require 'haml'
+require 'active_record'
+
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'mysql',
+  :host     => 'localhost',
+  :username => 'root',
+  :password => 'demo123',
+  :database => 'polla_development'
+)
+
+class Jugadore < ActiveRecord::Base
+end
 
 before do
   @nav = %w(inicio nosotros trabajos contacto)
 end
+
 
 get '/*' do
   @saludo = "Como estas"
   @path = request.env["REQUEST_PATH"].gsub(/\//, '')
   @path = 'inicio' if @path == ''
   template = :index
+
+  @jugadores = Jugadore.all
   case(@path)
     when ''
       template = :index
@@ -19,17 +36,29 @@ get '/*' do
     when 'trabajos'
       template = :trabajos
     when 'contacto'
+      @params = {}
       template = :contacto
   end
 
-  erb template
+  haml template
 end
 
 post '/contacto' do
- @path = 'contacto'
- erb :contacto
-end
+  
+  @params = params
 
+  mail = Mail.new do
+    subject 'Consulta ecuanime.net'
+    to 'boriscyber@gmail.com'
+    body @params['consulta']
+    from @params['email']
+  end
+
+  mail.deliver!
+
+  @path = 'contacto'
+  erb :contacto
+end
 
 
 #get '/*' do
